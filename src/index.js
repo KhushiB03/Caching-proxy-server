319de0a6b19589c543c6a26107f505617d93c0f9
KhushiB03 <ikhushi205@gmail.com>
2026-04-23 14:59:01 +0530

    ttl+ res from origin server

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import "dotenv/config";
 import yargs from "yargs"
 import {hideBin} from "yargs/helpers"
 import axios from "axios";
-
+import fs from "fs";
+import { url } from "inspector";
 
 const app = express();
 let count =0;
+
+//CLI ARGUMENTS
 const argv = yargs(hideBin(process.argv))
 .option("port",{
     type:"number",
@@ -18,51 +21,105 @@ const argv = yargs(hideBin(process.argv))
     demandOption:true,
     describe:"origin is server's URL"
 })
+.option("ttl",{
+    type:"number",
+    default:60,
+    describe:"cache TTL in seconds",
+})
 
 .help()
 .argv;
 const PORT = argv.port;
 const ORIGIN = argv.origin;
+const TTL = argv.ttl*1000;//convert to ms
 
+//load cache
+let cache={};
+const FILE = "cache.json";
+if(fs.existsSync(FILE)){
+    try{
+        cache =JSON.parse(fs.readFileSync(FILE , "utf-8"));
+    }catch(err){
+        console.error("error reading the cache file");
+        cache={};
+    }
+}
+//route
 app.get("/home/api" , (req,res)=>{
  
   console.log("/ page opened");
 
   res.send(`
-    number of times visited: ${count++}
-    
+    number of times visited: ${++count}
     PORT = ${PORT}
     origin : ${ORIGIN}
   `);
 });
-
-app.use(async (req,res)=>{
+//proxy+cache
+app.use(async(req , res)=>{
     try {
-       const targetURL = new URL(req.originalUrl , ORIGIN).toString() ;
-       console.log(`forwarding the request to ${targetURL}`);
-       //for get request
-       const response = await axios({
-        method: req.method,
-        url: targetURL,
-        headers:req.headers
-       })
-       ;
-       res.send(response.data);
-       console.log(response.data);
+        //take only get request
+        const isGet = req.method === "GET";
+        const targetUrl =new url(req.originalUrl ,ORIGIN).toString();
+        console.log(`final link :${targetUrl} ,
+            origin :${ORIGIN},
+        Server :${req.originalUrl}`);
 
+        //vache check 
+        if (isGet && cache[targetUrl]){
+            const entry = cache[targetUrl];
+            if(Date.now() <entry.expiry){
+                console.log("cache hit");
+                return res.send(entry.data);
+            }else{
+                console.log("cache expired");
+                delete cache[targetUrl];
+            }
+
+        }
+        console.log("cache mis");
+        const respone = await axios({
+            method:req.method,
+            url:targetUrl,
+            headers:{...req.headers , host:undefined},
+        });
+        //“Send a request to the original server (targetURL) and wait for the response.”
+        if (isGet){
+            cache[targetUrl] = {
+                data :respone.data,
+                expiry: Date.now()+TTL,
+            };
+            fs.writeFileSync(FILE , JSON.stringify(cache , null,2));
+        }
+        res.send(respone.data);
 
     } catch (error) {
-        res.status(500).send("Error forwarding request");
+        console.error("error found");
+        res.status(500).send("error forwarding request");
         
     }
-})
+});
+//auto cleanip
+setInterval(()=>{
+    const now = Date.now();
+    let changed = false;
+    for(const key in cache){
+        if(cache[key].expiry <now){
+            console.log("remove it" , key);
+            delete cache[key];
+            changed = true;
+        }
+
+    }
+    if(changed){
+        fs.writeFileSync(FILE , JSON.stringify(cache , null , 2));
+    }
+},50000);
 
 
 
-app.listen(3080,()=>{
-    console.log("chlaaaaaaaaaaaaaaaaaaaaa")
-})
 
+//server start
 app.listen(PORT , ()=>{
     console.log("PORT HAS STARTED RUNNING....");
     console.log(argv);
